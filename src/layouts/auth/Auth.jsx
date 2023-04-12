c134159e42059875fb4f865ba9f14b4229f42d18
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-04-12 21:02:12 +0200

    enhance .

--- src/layouts/auth/Auth.jsx
+++ src/layouts/auth/Auth.jsx
@@ -4,15 +4,13 @@ import guardAuth from "../../guard/guardAuth";
 
 function Auth() {
   return (
-    <>
-      <Container fluid>
-        <div className="width-40 mx-auto height-100 d-flex flex-column justify-content-center">
-          <h2 className="text-center mb-4">Notes App</h2>
-          {/* Pages */}
-          <Outlet />
-        </div>
-      </Container>
-    </>
+    <Container fluid>
+      <div className="width-40 mx-auto height-100 d-flex flex-column justify-content-center">
+        <h2 className="text-center mb-4">Notes App</h2>
+        {/* Pages */}
+        <Outlet />
+      </div>
+    </Container>
   );
 }
 
